b0587bffe4abbc4fa5feb2c072a790123705f2fe
Alejandro Rivera <alejandro.riveragut@gmail.com>
2026-07-16 16:09:05 -0400

    Implementación de logs

--- backend/tareas/src/main/java/com/to_do_list/tareas/service/TareaService.java
+++ backend/tareas/src/main/java/com/to_do_list/tareas/service/TareaService.java
@@ -8,6 +8,7 @@ import com.to_do_list.tareas.model.Tarea;
 import com.to_do_list.tareas.repository.TareaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TareaService {
 
     private final TareaRepository tareaRepository;
@@ -31,6 +33,7 @@ public class TareaService {
     @Transactional
     public TareaResponseDTO agregarTarea(TareaRequestDTO dto){
         Tarea tarea = new Tarea(null, dto.getNombre(), dto.getHoraInicio(), dto.getHoraTermino(), "PENDIENTE");
+        log.info("Tarea creada con exito");
         return tareaModelAssembler.toModel(tareaRepository.save(tarea));
     }
 
@@ -38,14 +41,16 @@ public class TareaService {
     public TareaResponseDTO modificarTarea(TareaRequestDTO dto, Long id){
         Optional<Tarea> tarea = tareaRepository.findById(id);
         if(tarea.isPresent()){
+            log.info("Tarea encontrada con id {} encontrada con exito!",id);
             return tarea.map(existente ->{
                 existente.setNombre(dto.getNombre());
                 existente.setHoraInicio(dto.getHoraInicio());
                 existente.setHoraTermino(dto.getHoraTermino());
-
+            log.info("Tarea modificada con exito!");
             return tareaModelAssembler.toModel(tareaRepository.save(existente));
             }).orElseThrow();
         }
+        log.warn("Tarea con id {} no encontrado",id);
         throw new TareaNotFoundException("Tarea con id "+id+"no encontrado");
     }
 
@@ -54,9 +59,12 @@ public class TareaService {
         Optional<Tarea> tarea = tareaRepository.findById(id);
 
         if(tarea.isPresent()){
+            log.info("Tarea encontrada con id {} encontrada con exito!",id);
             tareaRepository.deleteById(id);
+            log.info("Tarea eliminada con exito!");
             return;
         }
+        log.warn("Tarea con id {} no encontrado",id);
         throw new TareaNotFoundException("Tarea con id "+id+"no encontrado");
     }
 
@@ -65,11 +73,14 @@ public class TareaService {
         Optional<Tarea> tarea = tareaRepository.findById(id);
 
         if(tarea.isPresent()){
+            log.info("Tarea encontrada con id {} encontrada con exito",id);
             return tarea.map(existente ->{
                 existente.setEstado("COMPLETADO");
+                log.info("Estado cambiado con exito!");
                 return tareaModelAssembler.toModel(tareaRepository.save(existente));
             }).orElseThrow();
         }
+        log.warn("Tarea con id {} no encontrado",id);
         throw new TareaNotFoundException("Tarea con id "+id+"no encontrado");
     }
 
